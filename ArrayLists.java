fe8c5ac5e805ba9c60a11efd3feb648a00015770
MoogieMoo <yxia1@stuy.edu>
2017-04-30 15:54:31 -0400

    Update ArrayLists.java

--- ArrayLists.java
+++ ArrayLists.java
@@ -11,6 +11,7 @@ public class ArrayLists {
 	one.add( "dos" );
 	one.add( "tre" );
 	//print the array
+	System.out.println( "ArrayList one: " );
 	System.out.println( one );
 
 	//instantiate new arrayList with no wrapper
@@ -19,12 +20,15 @@ public class ArrayLists {
 	two.add( 2 );
 	two.add( 3 );
 	//print the array
+	System.out.println( "ArrayList two: " );
 	System.out.println( two );
-	//using add-at overridden add method to insert a zero at index 0
+	System.out.println( "Using overridden add method to insert a zero at index 0..." );
 	two.add( 0, 0 );
 	System.out.println( two );
-	two.set( 2, 9 );//changes value at index 2 (2) to 9
-	two.set( 3, 5 );//changes value at index 3 (3) to 5
+	System.out.println( "Change value at index 2 to 9..." );
+	two.set( 2, 9 );
+	System.out.println( "Change value at index 3 to 5..." );
+	two.set( 3, 5 );
 	System.out.println( two );
 	    
 	//instantiate new arrayList String wrapper
@@ -33,11 +37,15 @@ public class ArrayLists {
 	three.add( "err" );
 	three.add( "san" );
 	//print the array
+	System.out.println( "ArrayList three: " );
 	System.out.println( three );
 	//using add-at overridden add method to insert "lng" at index 0
+	System.out.println( "Using overridden add method to insert \"lng\" at index 0..." );
 	three.add( 0, "lng" );
-	three.set( 1, "eie" );//changes value at index 1 to  "eie"
-	three.set( 3, "sam" );//changes value at index 3 to "sam"
+	System.out.println( "Change value at index 3 to \"eie\"..." );
+	three.set( 1, "eie" );
+	System.out.println( "Change value at index 3 to \"sam\"..." );
+	three.set( 3, "sam" );
 	System.out.println( three );
 
     }
