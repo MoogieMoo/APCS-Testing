12fc3503dace628a0ce097f00d7f279eb75edcc7
MoogieMoo <yxia1@stuy.edu>
2017-04-29 23:59:06 -0400

    Update 2DArrays.java

--- 2DArrays.java
+++ 2DArrays.java
@@ -16,9 +16,9 @@ public class 2DArrays {
 	int[][] a = new int[4][4];
 	printArray( a );
 
-	int[][] b = { { 1, 2, 3 }
-		      { 4, 5, 6 }
-		      { 7, 8, 9 } };
+	int[][] b = { { 1, 2, 3 },
+		      { 4, 5, 6 },
+		      { 7, 8, 9 }, };
 	printArray( b );
 
 	System.out.println( "Array b's #rows:" + b.length );
