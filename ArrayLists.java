df5d366f40332bc7283e0ab02c030091f9887c6b
Yi Xia <yxia1@stuy.edu>
2017-04-30 11:56:25 -0400

    added test case

--- ArrayLists.java
+++ ArrayLists.java
@@ -13,6 +13,14 @@ public class ArrayLists {
 	//print the array
 	System.out.println( one );
 
+	//instantiate new arryList with no wrapper
+	List two = new ArrayList();
+	two.add( 1 );
+	two.add( 2 );
+	two.add( 3 );
+	//print the array
+	System.out.println( two );
+
     }
 
 }
