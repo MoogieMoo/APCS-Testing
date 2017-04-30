85e8f6e0cb15421877ba0901cb8e6f1095e5efe5
MoogieMoo <yxia1@stuy.edu>
2017-04-30 11:18:13 -0400

    Add files via upload

---
+++ ArrayLists.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayLists {
+
+    public static void main ( String[] args ) {
+
+	//instantiate new arrayList with no wrapper
+	List one = new ArrayList();
+	one.add( "uno" );
+	one.add( "dos" );
+	one.add( "tre" );
+	//print the array
+	System.out.println( one );
+
+    }
+
+}
