c47d84d6def64df9521a95de72b54164744e4e4f
MoogieMoo <yxia1@stuy.edu>
2017-05-01 17:07:17 -0400

    Update Loopy.java

--- Loopy.java
+++ Loopy.java
@@ -1,6 +1,9 @@
+import java.util.List;
+import java.util. ArrayList;
+
 public class Loopy {
 
-    public void printArray( String[][] array ) {
+    public static void printArray( String[][] array ) {
 	for ( int i = 0; i < array.length; i++ ) {
 	    for ( int j = 0; j < array[i].length; j++ ) {
 		System.out.print( array[i][j] + " " );
@@ -11,15 +14,23 @@ public class Loopy {
     
     public static void main ( String[] args ) {
 
-	List<String> loop1 = new ArrayList();
+	List<String> loop1 = new ArrayList(7);
+	loop1.add( "Sunday" );
+	loop1.add( "Monday" );
+	loop1.add( "Tuesday" );
+	loop1.add( "Wednesday" );
+	loop1.add( "Thursday" );
+	loop1.add( "Friday" );
+	loop1.add( "Saturday" );
 	String[][] loop2 = { { "cat", "bat", "mat" },
 			     { "cow", "bow", "mow" },
 			     { "cod", "bod", "mod" } };
 
 	System.out.println( "Using for loop (arrayList)..." );
-	System.out.println( "[" );
-	for ( int x = 0; x < loop1.size(); x ++ ) {
-	    System.out.print( loop.get(x) + " " );
+	System.out.print( "[ " );
+	for ( int x = 0; x < loop1.size(); x++ ) {
+	    System.out.print( loop1.get(x) );
+	    System.out.print( " " );
 	}
 	System.out.print( "]" );
 
@@ -37,14 +48,12 @@ public class Loopy {
 	printArray( loop2 );
 
 	System.out.println( "Using for-each loop (2D array)..." );
-	System.out.println( "[" );
 	for ( String[] b : loop2 ) {
 	    for ( String s : b ) {
 		System.out.print( s + " " );
 	    }
 	    System.out.println();
 	}
-	System.out.println( "]" );
 
     }
 
