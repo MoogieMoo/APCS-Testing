59af1e9cf770a64f77ad4e3e3dc6f01599563410
MoogieMoo <yxia1@stuy.edu>
2017-05-01 10:37:16 -0400

    Add files via upload

---
+++ Mathers.java
@@ -0,0 +1,17 @@
+public class Mathers {
+
+    public static void main ( String[] args ) {
+
+	System.out.println( "Finding the max of 3 and 7..." );
+	System.out.println( Math.max( 3, 7 ) );
+	System.out.println( "Finding the min of 4 and 1..." );
+	System.out.println( Math.min( 4, 1 ) );
+	System.out.println( "Finding the max of 2.6 and 5..." );
+	System.out.println( Math.max( 2.6, 5 ) );
+	System.out.println( "Finding the min of 9.9 and 6.8..." );
+	System.out.println( Math.min( 9.9, 6.8 ) );
+
+    }
+
+}
+			    
