95b0dce3b4af3f519ce8f018a21f964f86d6a113
Yi Xia <yxia1@stuy.edu>
2017-05-01 20:08:39 -0400

    Testing for Edwin

---
+++ Edwin/Car.java
@@ -0,0 +1,15 @@
+public interface Car {
+
+    public String getName();
+    
+    public String getBrand();
+ 
+    public int getPlate();
+
+    public void setName( String s );
+
+    public void setBrand( String s );
+
+    public void setPlate( int i );
+
+}
